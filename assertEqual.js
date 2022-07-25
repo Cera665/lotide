bc8948872c94cdf1abb7556e913e4f12a83c16c8
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-25 17:39:50 -0400

    Added assertEqual.js and head.js

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  }
+  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
