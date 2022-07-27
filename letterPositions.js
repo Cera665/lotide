127a5d9dcf51d2d7c0faddf1462420d8a046c966
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-27 18:05:06 -0400

    Added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
