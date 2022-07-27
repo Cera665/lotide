1cc7d99f196a439f1e12cfa3c5bb7ebc1d518edf
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-27 16:39:46 -0400

    Updated without.js

--- without.js
+++ without.js
@@ -1,11 +1,3 @@
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: "${array1}" === "${array2}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${array1}" !== "${array2}"`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -13,6 +5,14 @@ const eqArrays = function(array1, array2) {
     }
   }
   return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: "${array1}" === "${array2}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${array1}" !== "${array2}"`);
+  }
 };
 
 const without = (source, itemsToRemove) => {
