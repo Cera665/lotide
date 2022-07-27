aff423a7fd2918446dd11cadcb76694700aeaf94
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-26 20:46:51 -0400

    Added eqArrays.js and it worksgit add eqArrays.js Weeeeeee

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  }
+  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+
+
+// It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical. We will save this "deeper" problem for another day.
