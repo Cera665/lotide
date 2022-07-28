5ea60082448077b3f31502952b635d9925e5e0f1
Cera665 <deletedunreadxoxo@gmail.com>
2022-07-28 18:19:35 -0400

    Updated assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,10 +9,8 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: "${array1}" === "${array2}"`);   
+    console.log(`✅✅✅ Assertion Passed: "${array1}" === "${array2}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${array1}" !== "${array2}"`);
   }
-
-  console.log(`🛑🛑🛑 Assertion Failed: "${array1}" !== "${array2}"`);
-
 };
-console.log(assertArraysEqual([1, 2, 3], [1, 2]));
